e4acb0187bab4148ec7d762ff6f65d6329771569
Siti Ayuni <92244072+sitiayuni@users.noreply.github.com>
2021-10-26 15:57:59 +0700

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	/* NAMA  : SITI AYUNI
+	   KELAS : ILKOMP B
+	   NPM  : 2117051068
+	   */
+	 
+   int n_hari;
+   double rata_rata, jumlah;
+   
+   // input
+   cin >> n_hari;
+    for (int j=0; j<n_hari; j++){
+    	double n[j];
+    	cin >> n[j];
+    	jumlah += n[j];
+	}
+   cout << endl;
+   
+   rata_rata = jumlah / n_hari;
+   
+   // output
+   
+   cout << " Rata-rata : " << rata_rata << " ETH" << endl;
+   cout << " Total ETH : " << jumlah << " ETH"<< endl;
+   cout << " Dalam USD : $" << jumlah*4000 << endl;
+   
+	return 0;
+}
